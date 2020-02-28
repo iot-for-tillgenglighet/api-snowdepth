c42688d0b0aebb10e3640e30231bbae3e1df4e8e
Isak Styf <isak.styf@masarinconsulting.se>
2020-02-28 18:49:34 +0100

    Migrated to latest version of gqlgen and fixed some federation blockers

--- test/federation/gateway.js
+++ test/federation/gateway.js
@@ -1,6 +1,12 @@
 const { ApolloServer } = require('apollo-server');
 const { ApolloGateway } = require('@apollo/gateway');
 
+const axios = require('axios');
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 var port = process.env.API_GQL_FEDERATION_PORT
 var federatedServices = process.env.API_GQL_FEDERATION_SVCS
 
@@ -22,6 +28,7 @@ for (svc of federatedServices.split(",")) {
   console.info("Parsing svc config from: " + svc)
 
   svcConfig = svc.split(":")
+
   serviceList.push({
     name: svcConfig[0],
     url: "http://" + svcConfig[0] + ":" + svcConfig[1] + "/api/graphql"
@@ -30,11 +37,29 @@ for (svc of federatedServices.split(",")) {
 
 const gateway = new ApolloGateway({
     debug: true,
-    serviceList: serviceList
+    serviceList: serviceList,
   });
   
   (async () => {
     try {
+      // Start with checking if all services are available
+      var serviceFailedToRespond = false
+      for (service of serviceList) {
+        await axios.get(service.url + "/playground")
+                .then(response => {
+                  console.info(service.name + " is reachable.");
+                })
+                .catch(error => {
+                  serviceFailedToRespond = true
+                });
+        
+        if (serviceFailedToRespond) {
+          console.error(service.name + " is not reachable. Exiting!");
+          await sleep(3000)
+          process.exit()
+        }
+      }
+
       const { schema, executor } = await gateway.load();
   
       const server = new ApolloServer({ schema, executor });
@@ -46,7 +71,8 @@ const gateway = new ApolloGateway({
     catch(e)
     {
       console.error(e)
-      // Wait 5 seconds before exiting to prevent hammering of the the underlying services
-      setTimeout(process.exit(), 5000)
+      // Wait 5 seconds before exiting to prevent hammering of the underlying services
+      await sleep(5000);
+      process.exit()
     }
   })();
