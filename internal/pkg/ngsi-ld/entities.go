7ca3535643d86a3ee60c915590a69e5d32c52a89
Isak Styf <isak.styf@masarinconsulting.se>
2020-03-22 14:48:43 +0100

    Added support for querying snowdepths for a specific device
    * Also rewrote QueryEntities as a closure over an injected db interface
    * Yes, I know, we shouldn't have new features and refactorings in the same commit ...

--- internal/pkg/ngsi-ld/entities.go
+++ internal/pkg/ngsi-ld/entities.go
@@ -8,9 +8,14 @@ import (
 
 	"github.com/iot-for-tillgenglighet/api-snowdepth/internal/pkg/ngsi-ld/errors"
 	"github.com/iot-for-tillgenglighet/api-snowdepth/internal/pkg/ngsi-ld/types"
-	"github.com/iot-for-tillgenglighet/api-snowdepth/pkg/database"
 	"github.com/iot-for-tillgenglighet/api-snowdepth/pkg/models"
 )
+
+//entitiesDatastore is an interface containing all the entities related datastore functions
+type entitiesDatastore interface {
+	GetLatestSnowdepths() ([]models.Snowdepth, error)
+	GetLatestSnowdepthsForDevice(device string) ([]models.Snowdepth, error)
+}
 
 func convertDatabaseRecordToWeatherObserved(r *models.Snowdepth) *types.WeatherObserved {
 	if r != nil {
@@ -22,47 +27,78 @@ func convertDatabaseRecordToWeatherObserved(r *models.Snowdepth) *types.WeatherO
 	return nil
 }
 
-//QueryEntities handles GET requests for NGSI entitites
-func QueryEntities(w http.ResponseWriter, r *http.Request) {
-	entityTypes := r.URL.Query().Get("type")
+//NewQueryEntitiesHandler handles GET requests for NGSI entitites
+func NewQueryEntitiesHandler(db entitiesDatastore) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		entityTypes := r.URL.Query().Get("type")
 
-	if entityTypes != "" && entityTypes != "WeatherObserved" {
-		errors.ReportNewInvalidRequest(w, "This service only supports the WeatherObserved entity type.")
-		return
-	}
-
-	attributes := strings.Split(r.URL.Query().Get("attrs"), ",")
-	for _, attr := range attributes {
-		if attr != "" && attr != "snowHeight" {
-			errors.ReportNewInvalidRequest(w, "This service only supports the snowHeight attribute.")
+		if entityTypes != "" && entityTypes != "WeatherObserved" {
+			errors.ReportNewInvalidRequest(
+				w,
+				"This service only supports the WeatherObserved entity type.",
+			)
 			return
 		}
-	}
 
-	snowdepths, err := database.GetLatestSnowdepths()
-
-	if err != nil {
-		errors.ReportNewInternalError(w, "An internal error was encountered when trying to get entities from the database.")
-		return
-	}
-
-	depthcount := len(snowdepths)
-	ngsiEntities := []*types.WeatherObserved{}
-
-	if depthcount > 0 {
-		ngsiEntities = make([]*types.WeatherObserved, 0, depthcount)
-
-		for _, v := range snowdepths {
-			ngsiEntities = append(ngsiEntities, convertDatabaseRecordToWeatherObserved(&v))
+		attributes := strings.Split(r.URL.Query().Get("attrs"), ",")
+		for _, attr := range attributes {
+			if attr != "" && attr != "snowHeight" {
+				errors.ReportNewInvalidRequest(
+					w,
+					"This service only supports the snowHeight attribute.",
+				)
+				return
+			}
 		}
-	}
 
-	bytes, err := json.MarshalIndent(ngsiEntities, "", "  ")
-	if err != nil {
-		errors.ReportNewInternalError(w, "Failed to encode response.")
-		return
-	}
+		var snowdepths []models.Snowdepth
+		var err error
 
-	w.Header().Add("Content-Type", "application/ld+json")
-	w.Write(bytes)
+		query := r.URL.Query().Get("q")
+		refDevicePrefix := "refDevice==\""
+
+		if strings.HasPrefix(query, refDevicePrefix) {
+			splitElems := strings.Split(query, "\"")
+			deviceIDPrefix := "urn:ngsi-ld:Device:"
+			deviceID := splitElems[1]
+
+			if strings.HasPrefix(deviceID, deviceIDPrefix) {
+				deviceID = strings.TrimPrefix(deviceID, deviceIDPrefix)
+				snowdepths, err = db.GetLatestSnowdepthsForDevice(deviceID)
+			} else {
+				errors.ReportNewInvalidRequest(w, "Invalid device reference: "+deviceID)
+				return
+			}
+		} else {
+			snowdepths, err = db.GetLatestSnowdepths()
+		}
+
+		if err != nil {
+			errors.ReportNewInternalError(
+				w,
+				"An internal error was encountered when trying to get entities from the database.",
+			)
+			return
+		}
+
+		depthcount := len(snowdepths)
+		ngsiEntities := []*types.WeatherObserved{}
+
+		if depthcount > 0 {
+			ngsiEntities = make([]*types.WeatherObserved, 0, depthcount)
+
+			for _, v := range snowdepths {
+				ngsiEntities = append(ngsiEntities, convertDatabaseRecordToWeatherObserved(&v))
+			}
+		}
+
+		bytes, err := json.MarshalIndent(ngsiEntities, "", "  ")
+		if err != nil {
+			errors.ReportNewInternalError(w, "Failed to encode response.")
+			return
+		}
+
+		w.Header().Add("Content-Type", "application/ld+json")
+		w.Write(bytes)
+	})
 }
